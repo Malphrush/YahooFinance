14a020e888cda67bef378c44a0292f6247ed894c
Henry Malphrus <Malphrush@gmail.com>
2019-12-23 20:41:31 -0800

    small fix to main

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ from yahoofinance.query.MultiDataRequest import get_multi_hist_data
 
 
 if __name__ == '__main__':
-    tickers = ['APPL', 'MSFT', 'JPM']
-    result = get_multi_hist_data(tickers, nsimultaneous=1, logging=True)
-    print(result['NoExist'].columns())
+    tickers = ['AAPL', 'MSFT', 'JPM']
+    result = get_multi_hist_data(tickers)
     fields = ['Open', 'Close*']
     plot_hist_data(result, fields, tickers, nRows=1, nCols=2)
+
